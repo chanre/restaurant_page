97ec1d54cd1b03398bc6bb4aa873e5779cab2acf
Ryan <ryan.e.chan@gmail.com>
2024-04-03 22:49:49 -0400

    Add initial files, install webpack and add basic webpage

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+        title: 'My Restaurant Page',
+        filename: 'index.html',
+        inject: 'body',
+    }),
+  ],
+};
